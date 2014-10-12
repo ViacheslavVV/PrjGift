6c01ece30dc0053c1a4f06e4f34f3092a6a1e471
ViacheslavVV <voronovich_vv_12@mf.grsu.by>
2014-10-12 23:17:41 +0300

    LastUpd

--- Gift/Gift/Program.cs
+++ Gift/Gift/Program.cs
@@ -10,6 +10,24 @@ namespace Gift
     {
         static void Main(string[] args)
         {
+            Gift gift = new Gift();
+            
+            gift.AddComponent(new Candy("Candy", "qwe", 50, 52, 46, 2.9, "asd", "zxc"));
+            gift.AddComponent(new Cookie("Cookie", "quqi", 80, 60, 38, 8.4, "fgd", "cvb"));
+            gift.AddComponent(new Fruit("Fruit", "yui", 200, 56, 3, 9.2, "iop", "xcv"));
+
+            Console.Write("Components :"+Environment.NewLine +gift.ToString());
+
+            Console.WriteLine("Total weight : " + gift.GetWeight().ToString());
+
+            Console.WriteLine(Environment.NewLine + "Sorting......." + Environment.NewLine);
+
+            gift.SortingByWeight();
+
+            Console.Write("Components :"+Environment.NewLine + gift.ToString());
+
+            Console.WriteLine("Candy find : "+Environment.NewLine + gift.FindCandy(45.9, 46.1).ToString());
+            
         }
     }
 }
